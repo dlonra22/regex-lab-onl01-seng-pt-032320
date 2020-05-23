bef51190ffbe4e05ad9f4b74db59580fe402a720
arnie b <arnold.bwaila@gmail.com>
2020-05-23 13:57:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,9 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-    text.match(/\b(\D*[\d]){10}\b/)? true : false
+    text.match(/^[A-Z].*[!,?.]$/)? true : false
 end
 
 def valid_phone_number?(phone)
-  phone.match(/\b\d{10}\b/)? true : false
+  phone.match(/\b(\D*[\d]){10}\b/)? true : false
 end
